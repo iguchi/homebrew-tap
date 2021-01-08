cb031185b5c5a5b98b4032b0e4fcfa0975526eae
Takaaki Iguchi <iguchi@logly.co.jp>
2021-01-08 13:40:39 +0900

    feat: 🎸 add some formulas

---
+++ Formula/kustomize@3.8.8.rb
@@ -0,0 +1,60 @@
+class KustomizeAT388 < Formula
+  desc "Template-free customization of Kubernetes YAML manifests"
+  homepage "https://github.com/kubernetes-sigs/kustomize"
+  url "https://github.com/kubernetes-sigs/kustomize.git",
+      tag:      "kustomize/v3.8.8",
+      revision: "72262c5e7135045ed51b01e417a7e72f558a22b0"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes-sigs/kustomize.git"
+
+  livecheck do
+    url :head
+    regex(%r{kustomize/v?(\d+(?:\.\d+)+)$}i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    revision = Utils.safe_popen_read("git", "rev-parse", "HEAD").strip
+    tag = Utils.safe_popen_read("git", "tag", "--contains", "HEAD").strip
+
+    cd "kustomize" do
+      ldflags = %W[
+        -s -X sigs.k8s.io/kustomize/api/provenance.version=#{tag}
+        -X sigs.k8s.io/kustomize/api/provenance.gitCommit=#{revision}
+        -X sigs.k8s.io/kustomize/api/provenance.buildDate=#{Time.now.iso8601}
+      ]
+      system "go", "build", "-ldflags", ldflags.join(" "), "-o", bin/"kustomize"
+    end
+  end
+
+  test do
+    assert_match "kustomize/v#{version}", shell_output("#{bin}/kustomize version")
+
+    (testpath/"kustomization.yaml").write <<~EOS
+      resources:
+      - service.yaml
+      patchesStrategicMerge:
+      - patch.yaml
+    EOS
+    (testpath/"patch.yaml").write <<~EOS
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        selector:
+          app: foo
+    EOS
+    (testpath/"service.yaml").write <<~EOS
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        type: LoadBalancer
+    EOS
+    output = shell_output("#{bin}/kustomize build #{testpath}")
+    assert_match /type:\s+"?LoadBalancer"?/, output
+  end
+end
